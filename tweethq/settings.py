c985451a5cef206fbbff36cfd69de024efed2a00
Nitin Pilkhwal <1nasty.np@gmail.com>
2024-08-02 21:32:52 +0530

    Minor changes

--- tweethq/settings.py
+++ tweethq/settings.py
@@ -139,6 +139,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/accounts/login'
 
-LOGIN_REDIRECT_URL = '/tweet/'
-LOGOUT_REDIRECT_URL = '/tweet/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
   
